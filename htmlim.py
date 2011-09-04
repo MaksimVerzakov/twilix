2fecaf3882486ec036ebca96c8a28d9bad85bd00
Maxim <m2xver@gmail.com>
2011-09-04 15:48:39 +0700

    add docstring to htmlim

--- htmlim.py
+++ htmlim.py
@@ -1,17 +1,32 @@
+"""
+Realize elements of XHTML-IM protocol.
+"""
 from twilix.base import VElement
 from twilix import fields
 
 class HtmlHead(VElement):
+    """
+    Extends VElement class from twilix.base
+    Realize head tag of html.
+    """
     elementName = 'head'
 
     profile = fields.StringAttr('profile', required=False)
     title = fields.StringNode('title')
 
 class HtmlBody(VElement):
+    """
+    Extends VElement class from twilix.base
+    Realize body tag of html.
+    """
     elementName = 'body'
     elementUri = 'http://www.w3.org/1999/xhtml'
 
 class XHtmlIm(VElement):
+    """
+    Extends VElement class from twilix.base
+    Realize element according xhtml-im protocol.
+    """
     elementName = 'html'
     elementUri = 'http://jabber.org/protocol/xhtml-im'
 
