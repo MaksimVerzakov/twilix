29a7f42ddb0e77dcd54d9f47e46922bc4bf16bc7
rogov <rogov@hidevlab.com>
2012-06-06 16:41:19 +0700

    fix for Delay definition

---
+++ twilix/delay.py
@@ -0,0 +1,8 @@
+from twilix.base import VElement
+
+class Delay(VElement):
+    """
+    VElement-inheritor class for offline messages.
+    """
+    elementName = 'delay'
+    elementUri = 'urn:xmpp:delay'
--- twilix/muc/delay.py
+++
@@ -1,9 +0,0 @@
-from twilix.stanzas import Message
-
-class Delay(Message):
-    """
-    Message-inheritor class for chat history messages in multi-user chat.
-    """
-    # XXX: WTF?
-    elementName = 'delay'
-    elementUri = 'urn:xmpp:delay'
